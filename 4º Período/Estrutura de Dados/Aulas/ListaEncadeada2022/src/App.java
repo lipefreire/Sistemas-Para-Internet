e115e60350c70ed0a04d1efa36c543daff2da401
Felipe Freire <86164099+lipefreire@users.noreply.github.com>
2022-05-12 14:22:00 -0300

    Delete 4º Período/Estrutura de Dados/Aulas/ListaEncadeada2022 directory

--- 4º Período/Estrutura de Dados/Aulas/ListaEncadeada2022/src/App.java
+++
@@ -1,9 +0,0 @@
-
-public class App {
-	public static void main(String[] args) {
-		ListaEncadeada lista = new ListaEncadeada();
-		lista.adicionaNoInicio("SALGUEIRO");
-		lista.adicionaNoInicio("CABROBÓ");
-		System.out.println(lista);
-	}
-}
