643c1ebb04c9470297caf80eb15eafa3bb6ff25e
Felipe Freire <86164099+lipefreire@users.noreply.github.com>
2022-06-06 14:19:49 -0300

    Add files via upload

--- 4º Período/Engenharia de Software/Aulas/agenda/src/agenda/ContatosDAO.java
+++ 4º Período/Engenharia de Software/Aulas/agenda/src/agenda/ContatosDAO.java
@@ -18,4 +18,32 @@ public class ContatosDAO {
 		psts.setString(3, c.getDataCadastro());
 		psts.execute();
 	}
+	
+	public void atualizar(Contatos c) throws Exception {
+		String sql = "UPDATE contatos " + 
+				"SET nome = ?, idade = ?, dataCadastro = ? " + 
+				"WHERE id = ?;";
+		
+		Connection con = Conexao.ConexaoMySQL();
+		PreparedStatement psts = con.prepareStatement(sql);
+		
+		psts.setString(1, c.getNome());
+		psts.setInt(2, c.getIdade());
+		psts.setString(3, c.getDataCadastro());
+		psts.setInt(4, c.getId());
+		psts.execute();
+	}
+	
+	public void excluir(Contatos c)throws Exception {
+		String sql = "DELETE FROM contatos WHERE id = ?;";
+		
+		Connection con = Conexao.ConexaoMySQL();
+		PreparedStatement psts = con.prepareStatement(sql);
+		
+		psts.setString(1, c.getNome());
+		psts.setInt(2, c.getIdade());
+		psts.setString(3, c.getDataCadastro());
+		psts.setInt(4, c.getId());
+		psts.execute();
+	}
 }
--- 4º Período/Engenharia de Software/Aulas/agenda/src/agenda/app.java
+++ 4º Período/Engenharia de Software/Aulas/agenda/src/agenda/app.java
@@ -4,7 +4,21 @@ public class app {
 
 	public static void main(String[] args) throws Exception {
 		ContatosDAO contatosDAO = new ContatosDAO();
-
+		Contatos contato = new Contatos();
+		
+		//ATUALIZANDO UM CONTATO
+		contato.setNome("teste atualiza");
+		contato.setIdade(50);
+		contato.setDataCadastro("2022-06-01");
+		contato.setId(10);
+		contatosDAO.atualizar(contato);
+		
+		
+		//PARA EXCLUIR UM CONTATO
+		contato.setId(10);
+		contatosDAO.excluir(contato);
+		
+		//SALVANDO UM CONTATO
 		Contatos contatos = new Contatos();	
 		contatos.setNome("CONTATO 001");
 		contatos.setIdade(10);
