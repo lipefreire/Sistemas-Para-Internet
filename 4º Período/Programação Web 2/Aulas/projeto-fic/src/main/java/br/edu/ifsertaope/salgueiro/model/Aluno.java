fa4f66e542b7ba2d4b981f9e89bbc02c3b3a2cdf
Felipe Freire <86164099+lipefreire@users.noreply.github.com>
2022-06-07 17:13:56 -0300

    Add files via upload

---
+++ 4º Período/Programação Web 2/Aulas/projeto-fic/src/main/java/br/edu/ifsertaope/salgueiro/model/Aluno.java
@@ -0,0 +1,79 @@
+package br.edu.ifsertaope.salgueiro.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+@SuppressWarnings("serial")
+public class Aluno implements Serializable {
+	private int matricula;
+	private String nome, endereco, telefone;
+	private Date dataCadastro;
+	
+	public Aluno() {
+		
+	}
+
+	public Aluno(int matricula, String nome, String endereco, String telefone, Date dataCadastro) {
+		super();
+		this.matricula = matricula;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.dataCadastro = dataCadastro;
+	}
+	
+	public Aluno(String nome, String endereco, String telefone, Date dataCadastro) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.dataCadastro = dataCadastro;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [matricula=" + matricula + ", nome=" + nome + ", endereco=" + endereco + ", telefone=" + telefone
+				+ ", dataCadastro=" + dataCadastro + "]";
+	}
+	
+}
