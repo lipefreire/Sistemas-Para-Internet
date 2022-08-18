51d8d02d79b716c78bef4e227201adfa099acf6a
Felipe Freire <felipef12123@gmail.com>
2022-08-18 14:23:43 -0300

    arvores

---
+++ 4º Período/Estrutura de Dados/Aulas/Arvores/src/App.java
@@ -0,0 +1,19 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		Arvore arvore = new Arvore();
+		
+		arvore.inserir(10);
+		arvore.inserir(2);
+		arvore.inserir(80);
+		arvore.inserir(3);
+		arvore.inserir(11);
+		
+		if(arvore.busca(3) != null ) {
+			System.out.println("Elemento encontrado? " + arvore.busca(3).getElemento());
+			System.out.println("Pai: " + arvore.busca(3).getPai().getElemento());
+		}
+	}
+
+}
---
+++ 4º Período/Estrutura de Dados/Aulas/Arvores/src/Arvore.java
@@ -0,0 +1,99 @@
+public class Arvore {
+	public No raiz;
+	
+	public Arvore() {
+		this.raiz = null;
+	}
+	
+	// INSERINDO ELEMENTO
+	public void inserir(int elemento) {
+		if(this.isEmpty()) {
+			No novo = new No(elemento);
+			this.raiz = novo;
+		}
+		else {
+			No atual = this.raiz;
+			while(atual != null) {
+				if(elemento < atual.getElemento()) {
+					if(atual.getEsq() == null) {
+						No novo = new No(elemento);
+						atual.setEsq(novo);
+						novo.setPai(atual);
+						return;
+					}
+					else {
+						atual = atual.getEsq();
+					}
+				}
+				else {
+					if(atual.getDir() == null) {
+						No novo = new No(elemento);
+						atual.setDir(novo);
+						novo.setPai(atual);
+						return;
+					}
+					else {
+						atual = atual.getDir();
+					}
+				}
+			}
+		}
+	}
+	
+	public void inserirRec(int elemento) {
+		if(this.isEmpty()) {
+			No novo = new No(elemento);
+			this.raiz = novo;
+		}
+		else {
+			inserirRec(this.raiz, elemento);
+		}
+	}
+	
+	private void inserirRec(No atual, int elemento) {
+		if(elemento < atual.getElemento()) {
+			if(atual.getEsq() == null) {
+				No novo = new No(elemento);
+				atual.setEsq(novo);
+				novo.setPai(atual);
+				return;
+			}
+			else {
+				inserirRec(atual.getEsq(), elemento);
+			}
+		}
+		else {
+			if(atual.getDir() == null) {
+				No novo = new No(elemento);
+				atual.setDir(novo);
+				novo.setPai(atual);
+				return;
+			}
+		}
+	}
+	
+	// REMOVENDO ELEMENTO
+	
+	// MÉTODO DE BUSCA
+	public No busca(int elemento) {
+		No atual = this.raiz;
+		while(atual != null) {
+			if(atual.getElemento() == elemento) {
+				return atual;
+			}
+			else if(elemento < atual.getElemento()) {
+				atual = atual.getEsq();
+			}
+			else {
+				atual = atual.getDir();
+			}
+		}
+		return null;
+	}
+	
+	// VERIFICANDO SE ESTÁ VAZIo
+	public boolean isEmpty() {
+		return this.raiz == null;
+	}
+	
+}
